8075811285b61a4d9d64a2fd5df6f5a9d4459efb
Aleksey Smirnov <lirien42@gmail.com>
2019-04-28 18:10:58 +0300

    add play-pause

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log('It\'s O.K.');
+const player = document.querySelector('.player');
+const playPauseScreen = document.querySelector('.play-pause');
+// const progressBar = document.querySelector('.progress');
+// const muteButton = document.getElementById('mute-unmute');
+
+const togglePlayPause = () => {
+  if (player.paused) {
+    playPauseScreen.className = 'play-pause in-progress';
+    player.play();
+  } else {
+    playPauseScreen.className = 'play-pause paused';
+    player.pause();
+  }
+};
+
+playPauseScreen.addEventListener('click', togglePlayPause);
